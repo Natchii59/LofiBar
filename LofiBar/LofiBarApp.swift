2d0fb721979a97def6a7b38ed57faaf3351976d8
Natchii59 <caron.nathan@hotmail.com>
2025-05-23 11:35:25 +0200

    Initial Commit

---
+++ LofiBar/LofiBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  LofiBarApp.swift
+//  LofiBar
+//
+//  Created by Nathan Caron on 23/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct LofiBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
